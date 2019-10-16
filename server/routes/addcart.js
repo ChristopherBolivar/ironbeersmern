f6a0fcb7d18c0013123039004ac65111c467bebf
chris bolivar <christopherbolivar@hotmail.com>
2019-10-16 12:59:30 -0400

    correcting require models

--- server/routes/addcart.js
+++ server/routes/addcart.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { isLoggedIn } = require('../middlewares')
 const router = express.Router()
-const Beer = require('../models/beer')
+const Beer = require('../models/Beer')
 const User = require('../models/User')
 
 router.post('/add-cart', (req, res, next) => {
