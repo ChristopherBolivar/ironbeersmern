5b91cec3ecda7515693201abc7cfb058573bccff
chris bolivar <christopherbolivar@hotmail.com>
2019-10-16 13:08:09 -0400

    plz wok

--- server/bin/seeds.js
+++ server/bin/seeds.js
@@ -8,7 +8,7 @@ require('dotenv').config({ path: path.join(__dirname, '../.env') })
 
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
-const Beer = require('../models/beer')
+const Beer = require('../models/Beer')
 
 const bcryptSalt = 10
 
